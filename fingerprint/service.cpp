14b1249d25b53a19db06924ae9d3368e426d4efd
Arian <arian.kulmer@web.de>
2021-10-13 01:14:27 +0200

    ginkgo: fingerprint: Clang-format the code
    Change-Id: I78ca8f67846290826bd8c75c0db794bb5f477cde
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: fajar <santuyz321@gmail.com>

--- fingerprint/service.cpp
+++ fingerprint/service.cpp
@@ -25,6 +25,7 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
+using android::sp;
 using android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
 using android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint;
 
@@ -46,5 +47,5 @@ int main() {
 
     joinRpcThreadpool();
 
-    return 0; // should never get here
+    return 0;  // should never get here
 }
