bbf2ae8319869fce065c251cad57012061e3aba2
Arian <39061882+ArianK16a@users.noreply.github.com>
2021-12-04 02:16:31 +0000

    ginkgo: fingerprint: Uprev to 2.3 HAL
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>
    Change-Id: I56ea1646ef464f7e533add5a0017620e21db686d
    [fiqri19102002: Adapt to our fingerprint HAL and drop
    FOD support]
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: wisnuardhi28 <wisnuardi128@gmail.com>
    Signed-off-by: fajar <santuyz321@gmail.com>

--- fingerprint/BiometricsFingerprint.h
+++ fingerprint/BiometricsFingerprint.h
@@ -14,30 +14,32 @@
  * limitations under the License.
  */
 
-#ifndef ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_2_BIOMETRICSFINGERPRINT_H
-#define ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_2_BIOMETRICSFINGERPRINT_H
+#ifndef ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H
+#define ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H
 
 #include <hardware/fingerprint.h>
 
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
-#include <android/hardware/biometrics/fingerprint/2.2/IBiometricsFingerprint.h>
+#include <android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h>
 
 namespace android {
 namespace hardware {
 namespace biometrics {
 namespace fingerprint {
-namespace V2_2 {
+namespace V2_3 {
 namespace implementation {
 
-using ::android::sp;
-using ::android::hardware::hidl_string;
-using ::android::hardware::hidl_vec;
-using ::android::hardware::Return;
-using ::android::hardware::Void;
-using ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint;
+using ::android::hardware::biometrics::fingerprint::V2_1::FingerprintAcquiredInfo;
+using ::android::hardware::biometrics::fingerprint::V2_1::FingerprintError;
 using ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprintClientCallback;
 using ::android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
+using ::android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::hidl_string;
+using ::android::sp;
 
 struct BiometricsFingerprint : public IBiometricsFingerprint {
   public:
@@ -60,6 +62,9 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
     Return<RequestStatus> remove(uint32_t gid, uint32_t fid) override;
     Return<RequestStatus> setActiveGroup(uint32_t gid, const hidl_string& storePath) override;
     Return<RequestStatus> authenticate(uint64_t operationId, uint32_t gid) override;
+    Return<bool> isUdfps(uint32_t sensorId) override;
+    Return<void> onFingerDown(uint32_t x, uint32_t y, float minor, float major) override;
+    Return<void> onFingerUp() override;
 
   private:
     static fingerprint_device_t* openHal();
@@ -76,10 +81,10 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
 };
 
 }  // namespace implementation
-}  // namespace V2_2
+}  // namespace V2_3
 }  // namespace fingerprint
 }  // namespace biometrics
 }  // namespace hardware
 }  // namespace android
 
-#endif  // ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_2_BIOMETRICSFINGERPRINT_H
+#endif  // ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H
--- fingerprint/service.cpp
+++ fingerprint/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.2-service.ginkgo"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.ginkgo"
 
 #include <hidl/HidlTransportSupport.h>
 
@@ -25,8 +25,8 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
-using android::hardware::biometrics::fingerprint::V2_2::IBiometricsFingerprint;
-using android::hardware::biometrics::fingerprint::V2_2::implementation::BiometricsFingerprint;
+using android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
+using android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint;
 
 int main() {
     android::sp<IBiometricsFingerprint> service = BiometricsFingerprint::getInstance();
